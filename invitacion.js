3aa29640ada3e4580e39d709aff43c979e5b4724
rubeneduran0-ux <rubeneduran0@gmail.com>
2026-09-17 23:19:42 -0400

    Update invitacion.js

--- invitacion.js
+++ invitacion.js
@@ -1,18 +1,13 @@
 const enterButton = document.getElementById("enterButton");
-
 const inicio = document.querySelector(".hero");
-
 const letter = document.getElementById("letter");
-
 const paper = document.querySelector(".paper");
 
-
-// 🎵 MÚSICA DE LA INVITACIÓN
-const music = new Audio("musica/fiesta%20wao.mp3");
+// 🎵 MÚSICA DE LA INVITACIÓN (Asegúrate de que tu archivo dentro de /musica se llame fiesta.mp3)
+const music = new Audio("musica/fiesta.mp3");
 
 music.loop = true;
 music.volume = 0.6;
-
 
 // ✨ AL ENTRAR A LA NOCHE
 enterButton.addEventListener("click", () => {
@@ -22,31 +17,23 @@ enterButton.addEventListener("click", () => {
         console.log("No se pudo reproducir la música:", error);
     });
 
-
     // 🌑 Desaparece la pantalla inicial
     inicio.style.opacity = "0";
 
-
     setTimeout(() => {
-
         // Ocultar pantalla inicial
         inicio.style.display = "none";
 
-
         // 🦇 Mostrar la invitación
-        letter.style.position = "fixed";
         letter.style.visibility = "visible";
         letter.style.opacity = "1";
 
-
         document.body.style.background = "#17100b";
-
 
         // ✨ Animación del papel
         setTimeout(() => {
             paper.style.transform = "scale(1)";
         }, 100);
-
 
     }, 1200);
 
